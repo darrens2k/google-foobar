450be454d6df878e9a8672ed80f50b509aff9dcb
Darren Singh <darrennsingh@gmail.com>
2024-02-01 01:43:50 -0500

    finished level 4.1

--- Level 4/fourth_part1.py
+++ Level 4/fourth_part1.py
@@ -1,208 +1,88 @@
 
 def solution(banana_list):
     
-    # WHAT TO DO IF ODD NUMBER OF TRAINERS
+    # Function to check if a number is a power of 2
+    def isPowerOfTwo(n):
+        # Divide (integer division) by 2 until either # % 2 is 0 or the number reaches 0
+        if (n == 0):
+            return False
+        while (n != 1):
+            if (n % 2 != 0):
+                return False
+            # change to / if in python 2 or // if in python 3
+            n = n // 2
+    
+        return True
+    
+    # Function to play turns of thumbwar
+    def play_turns(x, y):
+        # if sum is odd they will be stuck in infinite battle
+        if (x + y) % 2 != 0: 
+            return 0
+        # if they are equal they will stop battling
+        if (x == y): 
+            return 1
+        # if the sum divided by the gcd is not a power of 2, then they stop battling
+        if isPowerOfTwo((x + y) // gcd(x, y)):
+            return 1
+        return 0
+
+    # Function to find gcd of two integers
     def gcd(a, b):
-        if a == 0:
-            return b
-        if b == 0:
-            return a
-        if a == b:
-            return a
-        if a > b:
-            return gcd(a - b, b)
-        return gcd(a, b - a)
-    print(gcd(1,4))
+        # ensure a is greater than or equal to b
+        if a < b:
+            a, b = b, a
+        
+        # euclidean algorithm
+        while b != 0:
+            a, b = b, a % b
+        
+        return a
 
-    # define base cases
-    
-    # if only 1 trainer
-    if len(banana_list) == 1:
-        return 1
-    
-    # if all trainers have the same amount of bananas
-    same_amount = True
-    prev = banana_list[0]
-    for value in banana_list:
-        if value != prev:
-            same_amount = False
-            break
-    
-    if same_amount:
-        return len(banana_list)
-
-    # if first element is odd, check to see if it can be paired with an even
-    # or if first element is even, check if it can be paired with an odd one
-    
-    # Function to locate an odd element within the input list
-    def find_odd(input_list):
-        # don't check the first element
-        for i in range(1, len(input_list)):
-            if input_list[i] % 2 != 0:
-                # return the index and value of the odd element found
-                return {'value':input_list[i], 'index':i}
-            
-        # no odd element found
-        return False
-    
-    # Function to locate an even element within the input list
-    def find_even(input_list):
-        # don't check the first element
-        for i in range(1, len(input_list)):
-            if input_list[i] % 2 == 0:
-                # return the index and value of the even element found
-                return {'value':input_list[i], 'index':i}
-            
-        # no even element found
-        return False
-    
-    # create a memory for memoization
-    mem = {}
-    
-    # Function to play a single turn
-    def play_turn(a, b, memory):
-        # check if we have seen this before
-        if (a, b) in memory:
-            return memory[(a,b)]
-        # nothing happens if a = b
-        if a == b:
-            return [-1, -1]
-        # a loses if it is bigger
-        if a > b:
-            memory[(a, b)] = [a - b, b + b]
-            return [a - b, b + b]
-        # b loses if it is bigger
-        else:
-            memory[(a, b)] = [a + a, b - a]
-            return [a + a, b - a]
-        
-    # Function to play turns with all other trainers
-    def play_turns(input_list, mem_dict):
-        # list to store outcomes of playing with each trainer
-        # first element; 1 = infinite, 0 = tie
-        # second element = index
-        outcomes = []
-        
-        # iterate through all other trainers
-        for i in range(1, len(input_list)):
-            # boolean to check if turn reached an infinite or tie state
-            not_done = True
-            # list to store outcomes of turns 
-            turns = []
-            # initialize first and second trainers
-            first = input_list[0]
-            second = input_list[i]
-            
-            # loop until terminal state reached
-            while not_done:
-                # play a turn
-                turn = play_turn(first, second, mem_dict)
-                # check if even/odd pair, infinite pair
-                if (turn[0] % 2 != 0 and turn[1] % 2 == 0) or (turn[0] % 2 == 0 and turn[1] % 2 != 0):
-                    not_done = False
-                    outcomes.append([1, i])
-                # if we return to a state we have seen before, likely that we are in infinite state
-                if turn in turns:
-                    not_done = False
-                    outcomes.append([1, i])
-                # if two trainers have same amount, we also stop
-                if turn == [-1, -1]:
-                    not_done = False
-                    outcomes.append([0, i])
-                # keep track of outcomes of turns
-                turns.append(turn)
-                # update first and second trainer values
-                first, second = turn
-                
-        return outcomes
-        
-    # create counters for trainers that leave and stay
-    stay = 0
-    leave = 0
-    
-    # trainers that form infinite pairs with no one
-    problems = []
-    
-    # copy of banana list to work on
-    copy_list = banana_list[:]
-    
-    # start iterating through the list
-    while len(copy_list) > 0:
-        # locate current trainer
-        current = copy_list[0]
-        
-        # check if the current trainer is odd, then find an even trainer and remove them both
-        next_even = False
-        next_odd = False
-        flag = False
-        if current % 2 != 0:
-            next_even = find_even(copy_list)
-            if next_even != False:
-                # this means we did find an even number
-                # remove current trainer and the one with the even number from the list
-                copy_list.pop(0)
-                copy_list.pop(next_even["index"] - 1)
-                # they are stuck fighting forever
-                stay += 2
+    # Function to compute amount of edges in maximum matches set
+    # Could also apply the blossom algorithm here
+    def leastRemainingNumbersToMatch(target, infinite_battles):
+        # dict to store the amount of times each number appears in an infinite battle
+        numbers = {}
+        for infinite in infinite_battles:
+            if infinite[0] in numbers:
+                numbers[infinite[0]] += 1
             else:
-                # if entering here, this means that no even number was found
-                # need to play turns on other elements in the list to find if another infinite pair can occur
-                # raise flag if this occurs
-                flag = True
-        else:
-            # current trainer is even
-            next_odd = find_odd(copy_list)
-            if next_odd != False:
-                # we did find an odd number
-                # remove them both from the list
-                copy_list.pop(0)
-                copy_list.pop(next_odd["index"] - 1)
-                # they will fight forever
-                stay += 2
+                numbers[infinite[0]] = 1
+            if infinite[1] in numbers:
+                numbers[infinite[1]] += 1
             else:
-                # no odd trainer found
-                # raise flag
-                flag = True
+                numbers[infinite[1]] = 1
                 
-        # check if flag was raised
-        if flag:
-            # this means no immediate pair was found that will be stuck forever
-            # need to play turns with other trainers to see if an infinite pair can be found
-            possibilites = play_turns(copy_list, mem)
-            
-            found_infinite = False
-            
-            # iterate through possibilities to see if an infinite pair was found
-            for i in range(len(possibilites)):
-                if possibilites[i][0] == 1:
-                    # found infinite pair, remove both trainers
-                    found_infinite = True
-                    copy_list.pop(0)
-                    copy_list.pop(possibilites[i][0] - 1)
-                    stay += 2
-                    break
-            
-            if not found_infinite:
-                # did not find infinite pair
-                # add trainer to problem list
-                problems.append(current)
-                # remove current element from banana list
-                copy_list.pop(0)
-                # print('no infinite partner found')
-                # if after banana list is empty, we have even number of problem items, add them to the ones that remain
-    
-    # now play turns for all problem trainers
-    print(problems)
+        # sort dictionary to locate numbers/trainers that are paired the least
+        numbers = sorted(numbers, key = lambda x: numbers[x])
+        created_pairs = set()
+        for n in numbers:
+            for infinite in infinite_battles:
+                # loop to create matches
+                if infinite[0] in created_pairs or infinite[1] in created_pairs:
+                    # if a trainer is in an already created pair, skip it
+                    continue
+                if n in infinite:
+                    created_pairs.add(infinite[0])
+                    created_pairs.add(infinite[1])
+        return target - len(created_pairs)
 
-    return leave + len(banana_list)
-    
-    
+    # set to store infinite battles
+    infinite_battles = set()
+    for i in range(len(banana_list) - 1):
+        for j in range(i + 1, len(banana_list)):
+            # play turns of all trainers
+            result = play_turns(banana_list[i], banana_list[j])
+            if result == 0:
+                # save to set if inifinite battle
+                infinite_battles.add((i, j))
 
+    return leastRemainingNumbersToMatch(len(banana_list), infinite_battles)
     
-    # basic rules
-    # if 1 is odd and 1 is even, this will be infinite
-    # if both are odd we must play the turn through
-    # if both are even either could happen
-    # divide both by 2 to see if it will be infinite
-    
-solution([1,7,3,21,13,19])
+solution([1, 7, 3, 21, 13, 19])
+solution([33553445, 987, 155, 94, 1, 47])
+solution([32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1])
+solution([7, 1, 7, 1, 7, 8])
+# solution([1,1,1,2])
+# ans is 2
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 
 def solution(banana_list):
-
-    # define base cases
     
-    # if only 1 trainer
+    # Base cases:
+    
+    # If there's only one trainer, return 1
     if len(banana_list) == 1:
         return 1
     
-    # if all trainers have the same amount of bananas
+    # If all trainers have the same number of bananas, return the number of trainers
     same_amount = True
     prev = banana_list[0]
     for value in banana_list:
@@ -18,9 +18,9 @@ def solution(banana_list):
     if same_amount:
         return len(banana_list)
     
-    # define a nested list to store possible pairings for each trainer
-    # 1 = infinite pairing, 0 = they will leave
-    # trainer cannot be paired with themself
+    # Initialize a nested list to store possible pairings for each trainer
+    # 1 represents infinite pairing, 0 represents they will leave
+    # Trainer cannot be paired with themself
     potentials = []
     for i in range(len(banana_list)):
         temp = []
@@ -28,178 +28,180 @@ def solution(banana_list):
             temp.append(0)
         potentials.append(temp)
     
-    # create a memory for memoization
+    # Create a memory for memoization
     mem = {}
     
     # Function to play a single turn
     def play_turn(a, b, memory):
-        # check if we have seen this before
+        # Check if we have seen this before
         if (a, b) in memory:
             return memory[(a,b)]
-        # nothing happens if a = b
+        # Nothing happens if a = b
         if a == b:
             return [-1, -1]
-        # a loses if it is bigger
+        # A loses if it is bigger
         if a > b:
             memory[(a, b)] = [a - b, b + b]
             return [a - b, b + b]
-        # b loses if it is bigger
+        # B loses if it is bigger
         else:
             memory[(a, b)] = [a + a, b - a]
             return [a + a, b - a]
         
     # Function to determine if two trainers will be in an infinite battle or not
     def play_turns(a, b, result, input_list, mem_dict):
-        # a and b are indices of the elements in the input list
+        # A and B are indices of the elements in the input list
         first = input_list[a]
         second = input_list[b]
         
-        # an odd/even pair will be stuck in an infinite battle
+        # An odd/even pair will be stuck in an infinite battle
         if (first % 2 == 0 and second % 2 != 0) or (first % 2 != 0 and second % 2 == 0):        
-            # update the outcomes list
+            # Update the outcomes list
             result[a][b] = 1
             return result
             
-        # play turns to check outcome
-        # boolean to check if turn reached an infinite or tie state
+        # Play turns to check outcome
+        # Boolean to check if turn reached an infinite or tie state
         not_done = True
-        # list to store outcomes of turns 
+        # List to store outcomes of turns 
         turns = []
-        # initialize counter
+        # Initialize counter
         counter = 0
         
-        # loop until terminal state reached, or 15 turns have past
+        # Loop until terminal state reached, or 15 turns have passed
         while not_done or counter == 15:
-            # play a turn
+            # Play a turn
             turn = play_turn(first, second, mem_dict)
-            # check if even/odd pair, infinite pair
+            # Check if even/odd pair, infinite pair
             if (turn[0] % 2 != 0 and turn[1] % 2 == 0) or (turn[0] % 2 == 0 and turn[1] % 2 != 0):
                 not_done = False
                 result[a][b] = 1
-            # if we return to a state we have seen before, likely that we are in infinite state
+            # If we return to a state we have seen before, likely that we are in infinite state
             if turn in turns:
                 not_done = False
                 result[a][b] = 1
-            # if two trainers have same amount, we also stop, not infinite pair
+            # If two trainers have the same amount, we also stop, not infinite pair
             if turn == [-1, -1]:
                 not_done = False
                 result[a][b] = 0
-            # keep track of outcomes of turns
+            # Keep track of outcomes of turns
             turns.append(turn)
-            # update first and second trainer values
+            # Update first and second trainer values
             first, second = turn
-            # update counter
+            # Update counter
             counter += 1
                 
         return result
         
-    # create counters for trainers that leave and stay
+    # Create counters for trainers that leave and stay
     stay = 0
     leave = 0
     
-    # trainers that form infinite pairs with no one
-    problems = []
-    
-    # copy of banana list to work on
-    copy_list = banana_list[:]
-    
-    # iterate through the input list
+    # Iterate through the input list
     for i in range(len(banana_list)):
-        # goal is to figure out what other trainers each trainer can be paired with
-        # nested loop is required
+        # Goal is to figure out what other trainers each trainer can be paired with
+        # Nested loop is required
         for j in range(len(banana_list)):
             if i != j:
-                # call function that will accept two trainers, and calculate whether or not they will be in an infinite battle
-                # don't call a trainer on itself
+                # Call function that will accept two trainers, and calculate whether or not they will be in an infinite battle
+                # Don't call a trainer on itself
                 potentials = play_turns(i, j, potentials, banana_list, mem)
-        
-    # figure out how many pairs each trainer can make
-    totals = []
-    for res in potentials:
-        totals.append(sum(res))
-        # if a single row contains all 0s, change to 200s
-        # this will ensure we recognize later on that it has no possible pairs
-        if sum(res) == 0:
-            for i in range(len(potentials)):
-                res[i] = 200
     
-    # create a pair for the trainer with smallest amount of potential pairs
-    # pick the option that appears the least for other trainers
+    # We have now created a graph that represents what trainers can be paired with what trainers
+    # We need to find the maximum possible amount of edges in this graph that can be connected
+    # This is called the maximal matching set
+    # The blossom algorithm can be used to find the maximal matching set a general graph
     
-    # function to check how many other trainers share these 1s
-    def check_ones(col):
-        # counter
-        count = 0
-        # iterate through the potentials matrix
-        for i in range(len(potentials)):
-            # check if each row shares the 1 located in the col
-            if potentials[i][col] == 1:
-                count += 1
-        # return amount of trainers that share that 1
-        return count
-    # loop through potentials
-    # boolean for loop condition
-    finished = False
-    while not finished:
-        print(totals)
-        # trainer with smallest amount of pairs
-        lowest = min(totals)
-        # index of this trainer (row of trainer in potentials)
-        loc = totals.index(lowest)
-        # check if the lowest row is 100s
-        if potentials[loc][0] >= 100:
-            # exit if this is the case
-            finished = True
-            break
-        # locate all the 1s in this row
-        # keep track of the one with lowest amount of occurrences
-        lowest = [0, 100]
-        ones = []
-        for i in range(len(potentials)):
-            if potentials[loc][i] == 1:
-                occurences = check_ones(i)
-                # ones = [col of 1, # of other trainers that share it]
-                ones.append([i, occurences])
-                # update lowest occurence location
-                if occurences < lowest[1]:
-                    lowest = [i, occurences]
-        
-        # remove this 1 and current trainer from the list
-        for i in range(len(potentials)):
-            if potentials[i][0] < 100:
-                # change the 1 to 0 for all trainers
-                potentials[i][lowest[0]] = 0
-                # change the current trainer to 0 for all trainers
-                potentials[i][loc] = 0
-                # since the current trainer is removed, replace its row with 100s
-                if i == loc:
-                    for j in range(len(potentials)):
-                        potentials[i][j] = 100
-        
-        # update the lowest value of totals to 100 so it is not picked again
-        totals[loc] = 100
-    
-    # if at the end we have only 100s in potentials, no trainers remain
-    # if we have 200s, then some trainers are left over
+    # Now we need to convert the matrix to a graph
+    # This can be done using a dictionary
+    graph = {}
     for i in range(len(potentials)):
-        if potentials[i][0] == 100:
-            stay += 1
-        else:
-            leave += 1
+        temp = []
+        for j in range(len(potentials)):
+            if potentials[i][j] == 1:
+                temp.append(j)
+        graph[i] = temp
     
-    for res in potentials:
-        print(res)
     
+    # Implement blossom algorithm
+    # Credit for Implementation: https://medium.com/@ckildalbrandt/demystifying-edmonds-blossom-algorithm-with-python-code-6353eb043311
+    def lca(match, base, p, a, b):
+        used = [False] * len(match)
+        while True:
+            a = base[a]
+            used[a] = True
+            if match[a] == -1:
+                break
+            a = p[match[a]]
+        while True:
+            b = base[b]
+            if used[b]:
+                return b
+            b = p[match[b]]
 
-    return leave
-    
-    
+    # Mark the path from v to the base of the blossom
+    def mark_path(match, base, blossom, p, v, b, children):
+        while base[v] != b:
+            blossom[base[v]] = blossom[base[match[v]]] = True
+            p[v] = children
+            children = match[v]
+            v = p[match[v]]
 
-    
-    # basic rules
-    # if 1 is odd and 1 is even, this will be infinite
-    # if both are odd we must play the turn through
-    # if both are even either could happen
-    # divide both by 2 to see if it will be infinite
-    
-solution([1,7,2])
+    def find_path(graph, match, p, root):
+        n = len(graph)
+        used = [False] * n
+        p[:] = [-1] * n
+        base = list(range(n))
+        used[root] = True
+        q = [root]
+
+        while q:
+            v = q.pop(0)
+            for to in graph[v]:
+                if base[v] == base[to] or match[v] == to:
+                    continue
+                if to == root or (match[to] != -1 and p[match[to]] != -1):
+                    curbase = lca(match, base, p, v, to)
+                    blossom = [False] * n
+                    mark_path(match, base, blossom, p, v, curbase, to)
+                    mark_path(match, base, blossom, p, to, curbase, v)
+                    for i in range(n):
+                        if blossom[base[i]]:
+                            base[i] = curbase
+                            if not used[i]:
+                                used[i] = True
+                                q.append(i)
+                elif p[to] == -1:
+                    p[to] = v
+                    if match[to] == -1:
+                        return to
+                    to = match[to]
+                    used[to] = True
+                    q.append(to)
+        return -1
+
+    # Implementation of Blossom Algorithm
+    def max_matching(graph):
+        n = len(graph)
+        match = [-1] * n
+        p = [0] * n
+        for i in range(n):
+            if match[i] == -1:
+                v = find_path(graph, match, p, i)
+                while v != -1:
+                    pv = p[v]
+                    ppv = match[pv]
+                    match[v] = pv
+                    match[pv] = v
+                    v = ppv
+        # Returns number of pairs in graph
+        return sum(1 for x in match if x != -1)
+
+    # Return the difference between the total number of trainers and the size of the maximal matching set
+    return len(banana_list) - max_matching(graph)
+
+# Test the function with a sample input
+# solution([1, 7, 3, 21, 13, 19])
+solution([33553445, 987, 155, 94, 1, 47])
+# solution([32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1, 32, 32, 32, 32, 32, 32, 32, 32, 1, 1])
+# The expected output for the provided input is 2
