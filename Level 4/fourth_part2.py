948b86198e3796e716663357ec5002ee49186e4c
Darren Singh <darrennsingh@gmail.com>
2024-02-02 21:20:29 -0500

    working on fourth level part 2`
    still need to write algorithm to find the optimal path

--- Level 4/fourth_part2.py
+++ Level 4/fourth_part2.py
@@ -2,6 +2,8 @@ def solution(entrances, exits, path):
 
     # at its core this is a graph theory problem
     # I am attempting to solve it without using a graph representation
+    # Goal is the find the path that maximizes the flow of bunnies
+    # I am more or less taking a heuristic approach
     
     # first figure out how many bunnies each state in the path can hold
     # also list to see how many bunnies are in each state
@@ -35,8 +37,16 @@ def solution(entrances, exits, path):
         return
     
     # iterate through path
+    # for i in range(len(path)):
+    #     for j in range(len(path)):
+    #         if path[i][j] != 0:
+    #             moveBunnies(i, j)
+
+    # Iterate through path
     for i in range(len(path)):
-        for j in range(len(path)):
+        # Sort the destinations by remaining capacity in descending order
+        destinations = sorted(range(len(path)), key=lambda x: path[i][x], reverse=True)
+        for j in destinations:
             if path[i][j] != 0:
                 moveBunnies(i, j)
 
@@ -58,4 +68,9 @@ solution([0, 1], [4, 5], [[0, 0, 4, 6, 0, 0],
                           [0, 0, 0, 0, 6, 6], 
                           [0, 0, 0, 0, 0, 0], 
                           [0, 0, 0, 0, 0, 0]])
+
+# solution([0], [3], [[0, 10, 20, 0], 
+#                     [0, 0, 0, 30], 
+#                     [0, 0, 0, 20], 
+#                     [0, 0, 0, 0]])
 
